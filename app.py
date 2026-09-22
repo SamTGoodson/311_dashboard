a8d60e56292c4a8e1395f0bff0251912771c58b3
SamTGoodson <samtgoodson@gmail.com>
2026-09-22 16:01:06 -0400

    formatting

--- app.py
+++ app.py
@@ -91,11 +91,23 @@ function(feature, layer) {
 #app
 app = DashProxy()
 app.layout = html.Div(children = [
-    html.H2('Daily 311 Complaints'),
-    html.P('This dashboard looks at 311 complaints by community board. ' \
-    'It updates every day with new complaints and takes a rolling average by complaint type. ' \
-    'It also ranks each community board by complaint type. Select a complaint type below to update the map (multiple can be selected), and click a community board to see its rolling average and rank.' \
-    'Below the map you can see a graph of complaint type by borough.',
+    html.H2('Daily 311 Complaints',           
+            style={
+               "textAlign": "center",
+               "fontFamily": "Georgia, serif"
+           }),
+    html.P(['This dashboard looks at 311 complaints by complaint type and by Community Board (CB). ' \
+    'It updates every day with new complaints, and it is on a day delay. ' ,
+    html.Br(),
+    html.Br(),
+    'For the map and graphs below it takes a 3 day rolling average by CB and complaint type, and it then ranks every CB by percentile on that complaint type. That percentile ranking is then mapped below. ' \
+    'Select a complaint type below to update the map (multiple complaints can be selected at one time), and click a CB to see its rolling average and rank. ' ,
+    html.Br(),
+    html.Br(),
+    'Below the map there are several graphs corresponding to the complaint type(s) you selected. ' \
+    'The first is of the community board with the largest change in rolling avg.  in the selected complaint type(s) during the time period covered in the data (eventually this will be 30 days). ' \
+    'Below that is a graph that allows you to select a CB and see how the rolling average of complaints has changed over the time represented in the data. ' \
+    'Finally, there is a bar chart of the sum of complaints by Borough. '],
            style={
                "textAlign": "center",
                "fontFamily": "Georgia, serif"
@@ -131,13 +143,42 @@ app.layout = html.Div(children = [
     html.Br(),
     html.Hr(),
     html.Br(),
+    html.P('This graph shows the CB with the largest change in the selected complaint type.',
+                      style={
+               "textAlign": "center",
+               "fontFamily": "Georgia, serif"
+           }),
     html.Div(
         dcc.Graph(
             figure={},
             id='biggest-shifts'
         )
     ),
-    html.P('Complaint type by Borough',
+    html.Br(),
+    html.Hr(),
+    html.Br(),
+    html.P('Pick any CB from the list to see how the complaint type selected above has changed over the period represented in the data.',
+                      style={
+               "textAlign": "center",
+               "fontFamily": "Georgia, serif"
+           }),
+    html.Div(
+        dcc.Dropdown(
+            id='cb-dropdown',
+            maxHeight=300,
+            options=[{"label": c.title(), "value": c} for c in nta_data["NTA"].unique()],
+            value='Jackson Heights'
+            ),
+            style={"marginLeft": "60px", "marginRight": "60px"}
+    ),
+    dcc.Graph(
+        figure={},
+        id='each-cb-graph'
+    ),   
+    html.Br(),
+    html.Hr(),
+    html.Br(),
+    html.P('This graph shows the sum of the selected complaint type by Borough.',
                       style={
                "textAlign": "center",
                "fontFamily": "Georgia, serif"
@@ -164,8 +205,50 @@ def update_graph(value_chosen):
         plot_df,
         x='borough',
         y='rolling_avg',
-        histfunc='sum'
+        histfunc='sum',
+        labels={
+                "borough": "Borough",
+                "rolling_avg": "3-Day Rolling Avg."
+                }
     )
+    fig.update_layout(
+    font_family="Georgia, serif",
+    font_color="black",
+    title_font_family="Georgia"
+    )
+
+    return fig
+
+# each cb callback
+@app.callback(
+    Output('each-cb-graph', 'figure'),
+    [Input('cat-dropdown', 'value'),
+     Input('cb-dropdown', 'value')]
+)
+def each_cb_graph(val1,val2):
+    df = cb_raw[cb_raw['complaint_type'].isin(val1)]
+    df = df.merge(nta_data)
+    df = df[df['NTA'] == val2]
+
+    NTA = df.iloc[0]['NTA']
+
+    fig = px.line(df, 
+                  x="date", 
+                  y="rolling_avg", 
+                  color = 'complaint_type',
+                  title=f"{val1} in {NTA}",
+                  labels={
+                     "date": "Date",
+                     "rolling_avg": "3-Day Rolling Avg.",
+                     "complaint_type": "Complaint Type"
+                 }
+                  )
+    fig.update_layout(
+    font_family="Georgia, serif",
+    font_color="black",
+    title_font_family="Georgia"
+)
+
 
     return fig
 
@@ -191,8 +274,25 @@ def shift_graph(value_chosen):
     highest_df = joined.loc[joined['shift'].abs().idxmax()]
     biggest_cb = highest_df['community_board']
     df = df[df['community_board'] == biggest_cb]
+    df = df.merge(nta_data)
+    NTA = df.iloc[0]['NTA']
 
-    fig = px.line(df, x="date", y="rolling_avg", title=f"{value_chosen} in {biggest_cb}")
+    fig = px.line(df, 
+                  x="date", 
+                  y="rolling_avg", 
+                  color = 'complaint_type',
+                  title=f"{value_chosen} in {NTA}",
+                  labels={
+                     "date": "Date",
+                     "rolling_avg": "3-Day Rolling Avg.",
+                     "complaint_type": "Complaint Type"
+                 }
+                  )
+    fig.update_layout(
+    font_family="Georgia, serif",
+    font_color="black",
+    title_font_family="Georgia"
+)
 
     return fig
 #map callback
